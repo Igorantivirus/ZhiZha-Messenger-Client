94c61cff9753792da40d456b0ea10251041f6a68
Игорь Антипов <igorantivirus@yandex.ru>
2026-02-18 14:58:41 +0700

    Панель чата теперь управляет списком чатов

--- src/App/UI/ChatPanel.hpp
+++ src/App/UI/ChatPanel.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "Core/Types.hpp"
 #include <RmlUi/Core/Element.h>
 #include <RmlUi/Core/ElementDocument.h>
+#include <string>
+#include <unordered_map>
 
 class ChatPanel
 {
@@ -10,16 +13,32 @@ public:
     {
         doc_ = &document;
         messanges_ = document.GetElementById("messages");
+        chatList_ = document.GetElementById("chat_list");
+    }
+
+    void initChatList(const std::map<IDType, std::string> &chats)
+    {
+        if (!isBound())
+            return;
+
+        chatList_->SetInnerRML("");
+        chatLabels_.clear();
+        activeElement = nullptr;
+
+        for (const auto &[id, name] : chats)
+            addChat(id, name, false);
     }
 
     bool isBound() const
     {
-        return doc_ && messanges_;
+        return doc_ && messanges_ && chatList_;
     }
 
-    void sendMessage(const std::string &userName, const std::string &text, bool isMe)
+    void sendMessage(const IDType chatID, const std::string &userName, const std::string &text, bool isMe)
     {
-        if(!isBound())
+        if (!isBound())
+            return;
+        if(chatID != activeChatID_)
             return;
 
         // <div class="msg-row">
@@ -27,7 +46,6 @@ public:
 
         row->SetClass("msg-row", true);
         row->SetClass(isMe ? "me" : "other", true);
-
 
         Rml::Element *msgStack = row->AppendChild(doc_->CreateElement("div"));
         msgStack->SetClass("msg-stack", true);
@@ -45,7 +63,7 @@ public:
 
     void clearMessanges()
     {
-        if(messanges_)
+        if (messanges_)
             messanges_->SetInnerRML("");
     }
 
@@ -54,8 +72,68 @@ public:
         return messanges_ ? messanges_->GetInnerRML() : std::string{};
     }
 
-private:
+    bool updateClickEvent(const std::string& id, const Rml::Element* element)
+    {
+        auto found = chatLabels_.find(&id);
+        if(found != chatLabels_.end())
+        {
+            makeChatActibe(id, found->second);
+            return true;
+        }
+        return false;
+    }
 
+    const Rml::Element *getActiveElement() const
+    {
+        return activeElement;
+    }
+
+    const IDType getActiveChatID() const
+    {
+        return activeChatID_;
+    }
+
+private:
     Rml::ElementDocument *doc_ = nullptr;
     Rml::Element *messanges_ = nullptr;
+    Rml::Element *chatList_ = nullptr;
+
+    // By string id
+    std::unordered_map<const std::string *, Rml::Element *> chatLabels_;
+    Rml::Element *activeElement = nullptr;
+    IDType activeChatID_ = 0;//aero -- is invalid
+
+private:
+    void addChat(const IDType id, const std::string &name, bool active)
+    {
+        /*
+        <div class="chat-item active">
+            <div class="chat-title">Общий</div>
+            <div class="chat-last">Последнее: «Ок, понял»</div>
+        </div>
+        */
+
+        Rml::Element *chatItem = chatList_->AppendChild(doc_->CreateElement("div"));
+        chatItem->SetClass("chat-item", true);
+        chatItem->SetClass("active", active);
+        chatItem->SetId(std::to_string(id));
+
+        Rml::Element *chatTitle = chatItem->AppendChild(doc_->CreateElement("div"));
+        chatTitle->SetClass("chat-title", true);
+        chatTitle->SetInnerRML(name);
+
+        Rml::Element *chatLast = chatItem->AppendChild(doc_->CreateElement("div"));
+        chatTitle->SetClass("chat-last", true);
+
+        chatLabels_[&chatItem->GetId()] = chatItem;
+    }
+
+    void makeChatActibe(const std::string& id, Rml::Element* chat)
+    {
+        if(activeElement)
+            activeElement->SetClass("active", false);
+        activeElement = chat;
+        activeElement->SetClass("active", true);
+        activeChatID_ = std::stoul(id);
+    }
 };
