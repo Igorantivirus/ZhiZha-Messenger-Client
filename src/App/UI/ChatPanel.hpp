d70b4df23a717d6d377a465b4364e242cce8f155
Игорь Антипов <igorantivirus@yandex.ru>
2026-02-22 17:31:30 +0700

    Обновление при создании чата

--- src/App/UI/ChatPanel.hpp
+++ src/App/UI/ChatPanel.hpp
@@ -14,7 +14,7 @@ public:
         doc_ = &document;
         messanges_ = document.GetElementById("messages");
         chatList_ = document.GetElementById("chat_list");
-        currentChatName_  = document.GetElementById("chat_name");
+        currentChatName_ = document.GetElementById("chat_name");
     }
 
     void initChatList(const std::map<IDType, std::string> &chats)
@@ -39,7 +39,7 @@ public:
     {
         if (!isBound())
             return;
-        if(chatID != activeChatID_)
+        if (chatID != activeChatID_)
             return;
 
         // <div class="msg-row">
@@ -73,10 +73,10 @@ public:
         return messanges_ ? messanges_->GetInnerRML() : std::string{};
     }
 
-    bool updateClickEvent(const std::string& id, const Rml::Element* element)
+    bool updateClickEvent(const std::string &id, const Rml::Element *element)
     {
         auto found = chatLabels_.find(&id);
-        if(found != chatLabels_.end())
+        if (found != chatLabels_.end())
         {
             makeChatActive(id, found->second);
             return true;
@@ -94,6 +94,11 @@ public:
         return activeChatID_;
     }
 
+    void chatCreated(const IDType chatID, const std::string& name)
+    {
+        addChat(chatID, name, false);
+    }
+
 private:
     Rml::ElementDocument *doc_ = nullptr;
     Rml::Element *messanges_ = nullptr;
@@ -103,12 +108,12 @@ private:
     std::unordered_map<const std::string *, Rml::Element *> chatLabels_;
     Rml::Element *activeChatLabel_ = nullptr;
     Rml::Element *currentChatName_ = nullptr;
-    IDType activeChatID_ = 0;//aero -- is invalid
-
+    IDType activeChatID_ = 0; // aero -- is invalid
 
     std::unordered_map<IDType, std::string> chatMessages_;
 
 private:
+
     void addChat(const IDType id, const std::string &name, bool active)
     {
         /*
@@ -133,31 +138,29 @@ private:
         chatLabels_[&chatItem->GetId()] = chatItem;
     }
 
-    void makeChatActive(const std::string& id, Rml::Element* chat)
+    void makeChatActive(const std::string &id, Rml::Element *chat)
     {
-        if(!isBound())
+        if (!isBound())
             return;
 
-        if(activeChatLabel_)
+        if (activeChatLabel_)
             activeChatLabel_->SetClass("active", false);
-        if(activeChatID_ != 0)
+        if (activeChatID_ != 0)
             chatMessages_[activeChatID_] = messanges_->GetInnerRML();
-        
 
         activeChatLabel_ = chat;
         activeChatLabel_->SetClass("active", true);
         activeChatID_ = std::stoul(id);
         messanges_->SetInnerRML(chatMessages_[activeChatID_]);
 
-        for(std::size_t i = 0; i < activeChatLabel_->GetNumChildren(); ++i)
+        for (std::size_t i = 0; i < activeChatLabel_->GetNumChildren(); ++i)
         {
             auto elem = activeChatLabel_->GetChild(i);
-            if(elem->IsClassSet("chat-title"))
+            if (elem->IsClassSet("chat-title"))
             {
                 currentChatName_->SetInnerRML(elem->GetInnerRML());
                 break;
             }
         }
-        
     }
 };
